f7971be6c1cd06e978b09294c4fa67151fe7b7d8
jeffi <neethu.bc89@gmail.com>
2023-07-06 07:19:52 +0530

    added webmvcConfigurer to handle cors

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/", allowCredentials = "true")
 public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world")
