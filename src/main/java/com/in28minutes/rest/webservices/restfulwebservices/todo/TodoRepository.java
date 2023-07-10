b5a717c26a1e125ca361e6932d4c9f2530c23172
jeffi <neethu.bc89@gmail.com>
2023-07-10 10:07:37 +0530

    h2 and jpa

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoRepository.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoRepository.java
@@ -3,6 +3,10 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+     List<Todo> findByUsername(String name);
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoService.java
@@ -9,52 +9,63 @@ import org.springframework.stereotype.Service;
 @Service
 public class TodoService {
 	
-	private static List<Todo> todos = new ArrayList<>();
-	
+//	private static List<Todo> todos = new ArrayList<>();
+
+	private TodoRepository repository;
+
+	public TodoService(TodoRepository repository) {
+		this.repository = repository;
+	}
+
 	private static int todosCount = 0;
 
 	
-	static {
-		todos.add(new Todo(++todosCount, "Neethu","Get AWS Certified",
-							LocalDate.now().plusYears(10), false ));
-		todos.add(new Todo(++todosCount, "Neethu","Learn DevOps",
-				LocalDate.now().plusYears(11), false ));
-		todos.add(new Todo(++todosCount, "Neethu","Learn Full Stack Development",
-				LocalDate.now().plusYears(12), false ));
-		todos.add(new Todo(++todosCount, "Jeffi","Learn DevOps R",
-				LocalDate.now().plusYears(11), false ));
-		todos.add(new Todo(++todosCount, "Jeffi","Learn Full Stack Development R",
-				LocalDate.now().plusYears(12), false ));
-		todos.add(new Todo(++todosCount, "Cristal","Learn ABC",
-				LocalDate.now().plusYears(1), false ));
-
-	}
+//	static {
+//		todos.add(new Todo(++todosCount, "Neethu","Get AWS Certified",
+//							LocalDate.now().plusYears(10), false ));
+//		todos.add(new Todo(++todosCount, "Neethu","Learn DevOps",
+//				LocalDate.now().plusYears(11), false ));
+//		todos.add(new Todo(++todosCount, "Neethu","Learn Full Stack Development",
+//				LocalDate.now().plusYears(12), false ));
+//		todos.add(new Todo(++todosCount, "Jeffi","Learn DevOps R",
+//				LocalDate.now().plusYears(11), false ));
+//		todos.add(new Todo(++todosCount, "Jeffi","Learn Full Stack Development R",
+//				LocalDate.now().plusYears(12), false ));
+//		todos.add(new Todo(++todosCount, "Cristal","Learn ABC",
+//				LocalDate.now().plusYears(1), false ));
+//
+//	}
 	
 	public List<Todo> findByUsername(String username){
-		Predicate<? super Todo> predicate = 
-				todo -> todo.getUsername().equalsIgnoreCase(username);
-		return todos.stream().filter(predicate).toList();
+//		Predicate<? super Todo> predicate =
+//				todo -> todo.getUsername().equalsIgnoreCase(username);
+//		return todos.stream().filter(predicate).toList();
+		return repository.findByUsername(username);
 	}
 	
 	public Todo addTodo(String username, String description, LocalDate targetDate, boolean done) {
-		Todo todo = new Todo(++todosCount,username,description,targetDate,done);
-		todos.add(todo);
+		Todo todo = new Todo(null,username,description,targetDate,done);
+//		todos.add(todo);
+		repository.save(todo);
 		return todo;
 	}
 	
 	public void deleteById(int id) {
-		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
-		todos.removeIf(predicate);
+//		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+//		todos.removeIf(predicate);
+		repository.deleteById(id);
 	}
 
 	public Todo findById(int id) {
-		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
-		Todo todo = todos.stream().filter(predicate).findFirst().get();
-		return todo;
+//		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+//		Todo todo = todos.stream().filter(predicate).findFirst().get();
+//		return todo;
+		return repository.findById(id).get();
 	}
 
 	public void updateTodo(Todo todo) {
-		deleteById(todo.getId());
-		todos.add(todo);
+//		deleteById(todo.getId());
+//		todos.add(todo);
+		repository.save(todo);
 	}
 }
